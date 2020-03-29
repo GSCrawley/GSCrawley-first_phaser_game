6c41e46bb5320c7c32a36fd2a3a7a1ec2da7c453
GSCrawley <gscrawley@gmail.com>
2020-03-28 18:15:41 -0700

    loading and playing audio in an event

--- game.js
+++ game.js
@@ -1,41 +1,43 @@
 const gameState = {}
 
 function preload() {
-  this.load.image('codey', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/codey.png');
+  // load our 'incredible' sound here!
+    this.load.audio('incredible', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/incredible.mp3');
+    this.load.audio('awesome', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/reallyawesome.mp3')
 }
 
 function create() {
-  gameState.codey = this.add.sprite(150, 200, 'codey');
-  gameState.cursors = this.input.keyboard.createCursorKeys();
-}
-  // Set cursor keys here!
+  // add our 'incredible' sound to our scene here!
+  gameState.incredible = this.sound.add('incredible')
+  gameState.awesome = this.sound.add('awesome')
+  
+  // Display "Incredible" and "Really, really awesome" buttons
+  gameState.incredibleBox = this.add.rectangle(200, 150, 300, 200, 0xdadaaa)
+  gameState.awesomeBox = this.add.rectangle(200, 400, 300, 200, 0xaadada)
+  gameState.incredibleText = this.add.text(150, 135, "Incredible", { fill: "#222222", font: "20px Times New Roman"})
+  gameState.awesomeText = this.add.text(110, 385, "Really, really awesome", { fill: "#222222", font: "20px Times New Roman"})
+  gameState.incredibleBox.setInteractive();
+  gameState.awesomeBox.setInteractive();
 
-function update() {
-  // Update based on keypress here!
-  if (gameState.cursors.right.isDown) {
-    gameState.codey.x += 5;
-  }
-  if (gameState.cursors.left.isDown) {
-    gameState.codey.x -= 5;
-  }
-  if (gameState.cursors.up.isDown) {
-    gameState.codey.y -= 5;
-  }
-  if (gameState.cursors.down.isDown) {
-    gameState.codey.y += 5;
-  }
+  // add a 'pointerup' handler to incredibleBox here:
+  gameState.incredibleBox.on('pointerup',
+function() {
+  gameState.incredible.play()
+  });
+  gameState.awesomeBox.on('pointerup', function() {
+  gameState.awesome.play()
+  })
 }
 
 const config = {
-	type: Phaser.AUTO,
-	width: 400,
-	height: 500,
-	backgroundColor: "#5f2a55",
-	scene: {
+  type: Phaser.AUTO,
+  width: 400,
+  height: 600,
+  backgroundColor: "#333333",
+  scene: {
     preload,
-    create,
-    update
-	}
-};
+    create
+  }
+}
 
-const game = new Phaser.Game(config);
+const game = new Phaser.Game(config)
