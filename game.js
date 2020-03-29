e87f1724aa866075e29bb11b74085eea14dafbd5
GSCrawley <gscrawley@gmail.com>
2020-03-28 17:29:04 -0700

    made states interactive and added event listeners to switch colors on mouseclick

--- game.js
+++ game.js
@@ -1,24 +1,40 @@
-const gameState = {};
-
-function create() {
-  gameState.circle = this.add.circle(40, 100, 10, 0xff9999)
-}
-
-function update() {
-  gameState.circle.y += 1
-}
-
-const config = {
-	type: Phaser.AUTO,
-	width: 450,
-	height: 600,
-	backgroundColor: "#99ff99",
-	scene: {
-    create,
-    update
-	}
-}
-
-const game = new Phaser.Game(config)
-
+const gameState = {
+    onColor: 0xaaffaa,
+    offColor: 0xffaaaa
+  }
   
+  function create() {
+    gameState.rect1 = this.add.rectangle(200, 100, 100, 100, gameState.onColor);
+    gameState.rect2 = this.add.rectangle(200, 300, 100, 100, gameState.offColor);
+    gameState.rect1.setInteractive();
+    gameState.rect2.setInteractive();
+    gameState.rect2.on('pointerup', 
+  function() {
+    gameState.rect1.fillColor = gameState.offColor;
+    gameState.rect2.fillColor = gameState.onColor
+    });
+    gameState.rect1.on('pointerup',
+  function() {
+    gameState.rect1.fillColor =
+  gameState.onColor;
+    gameState.rect2.fillColor =
+  gameState.offColor
+    });
+  }
+    // add the switchedOn state here
+    
+    // set gameState.rect2 as interactive here
+    
+    // create the pointerup listener for rect2 here
+    
+  const config = {
+    type: Phaser.AUTO,
+    width: 400,
+    height: 450,
+    backgroundColor: 0x333333,
+    scene: {
+      create
+    }
+  }
+  
+  const game = new Phaser.Game(config)
