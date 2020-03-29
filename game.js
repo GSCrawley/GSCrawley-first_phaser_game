641a8d31a84352f06e78e8c88c243a122058c721
GSCrawley <gscrawley@gmail.com>
2020-03-28 18:48:32 -0700

    learned Phaser basics

--- game.js
+++ game.js
@@ -1,43 +1,39 @@
 const gameState = {}
 
 function preload() {
-  // load our 'incredible' sound here!
-    this.load.audio('incredible', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/incredible.mp3');
-    this.load.audio('awesome', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/reallyawesome.mp3')
+  this.load.image('codey', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/codey.png');
 }
 
 function create() {
-  // add our 'incredible' sound to our scene here!
-  gameState.incredible = this.sound.add('incredible')
-  gameState.awesome = this.sound.add('awesome')
-  
-  // Display "Incredible" and "Really, really awesome" buttons
-  gameState.incredibleBox = this.add.rectangle(200, 150, 300, 200, 0xdadaaa)
-  gameState.awesomeBox = this.add.rectangle(200, 400, 300, 200, 0xaadada)
-  gameState.incredibleText = this.add.text(150, 135, "Incredible", { fill: "#222222", font: "20px Times New Roman"})
-  gameState.awesomeText = this.add.text(110, 385, "Really, really awesome", { fill: "#222222", font: "20px Times New Roman"})
-  gameState.incredibleBox.setInteractive();
-  gameState.awesomeBox.setInteractive();
-
-  // add a 'pointerup' handler to incredibleBox here:
-  gameState.incredibleBox.on('pointerup',
-function() {
-  gameState.incredible.play()
-  });
-  gameState.awesomeBox.on('pointerup', function() {
-  gameState.awesome.play()
-  })
+  gameState.codey = this.add.sprite(150, 200, 'codey');
+  gameState.cursors = this.input.keyboard.createCursorKeys()
 }
 
-const config = {
-  type: Phaser.AUTO,
-  width: 400,
-  height: 600,
-  backgroundColor: "#333333",
-  scene: {
-    preload,
-    create
+function update() {
+  if (gameState.cursors.down.isDown) {
+    gameState.codey.y += 2;
+  }
+  if (gameState.cursors.up.isDown) {
+    gameState.codey.y -= 2;
+  }
+  if (gameState.cursors.left.isDown) {
+    gameState.codey.x -= 2;
+  }
+  if (gameState.cursors.right.isDown) {
+    gameState.codey.x += 2;
   }
 }
 
-const game = new Phaser.Game(config)
+const config = {
+	type: Phaser.AUTO,
+	width: 400,
+	height: 500,
+	backgroundColor: "0xdda0dd",
+	scene: {
+	  preload,
+	  create,
+    update
+	}
+}
+
+const game = new Phaser.Game(config);
