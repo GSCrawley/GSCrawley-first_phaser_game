7e33fa01e02b23a33fa13bf7439db4da8fa38cca
GSCrawley <gscrawley@gmail.com>
2020-03-28 17:55:50 -0700

    keyboard input

--- game.js
+++ game.js
@@ -1,40 +1,41 @@
-const gameState = {
-    onColor: 0xaaffaa,
-    offColor: 0xffaaaa
+const gameState = {}
+
+function preload() {
+  this.load.image('codey', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/codey.png');
+}
+
+function create() {
+  gameState.codey = this.add.sprite(150, 200, 'codey');
+  gameState.cursors = this.input.keyboard.createCursorKeys();
+}
+  // Set cursor keys here!
+
+function update() {
+  // Update based on keypress here!
+  if (gameState.cursors.right.isDown) {
+    gameState.codey.x += 5;
   }
-  
-  function create() {
-    gameState.rect1 = this.add.rectangle(200, 100, 100, 100, gameState.onColor);
-    gameState.rect2 = this.add.rectangle(200, 300, 100, 100, gameState.offColor);
-    gameState.rect1.setInteractive();
-    gameState.rect2.setInteractive();
-    gameState.rect2.on('pointerup', 
-  function() {
-    gameState.rect1.fillColor = gameState.offColor;
-    gameState.rect2.fillColor = gameState.onColor
-    });
-    gameState.rect1.on('pointerup',
-  function() {
-    gameState.rect1.fillColor =
-  gameState.onColor;
-    gameState.rect2.fillColor =
-  gameState.offColor
-    });
+  if (gameState.cursors.left.isDown) {
+    gameState.codey.x -= 5;
   }
-    // add the switchedOn state here
-    
-    // set gameState.rect2 as interactive here
-    
-    // create the pointerup listener for rect2 here
-    
-  const config = {
-    type: Phaser.AUTO,
-    width: 400,
-    height: 450,
-    backgroundColor: 0x333333,
-    scene: {
-      create
-    }
+  if (gameState.cursors.up.isDown) {
+    gameState.codey.y -= 5;
   }
-  
-  const game = new Phaser.Game(config)
+  if (gameState.cursors.down.isDown) {
+    gameState.codey.y += 5;
+  }
+}
+
+const config = {
+	type: Phaser.AUTO,
+	width: 400,
+	height: 500,
+	backgroundColor: "#5f2a55",
+	scene: {
+    preload,
+    create,
+    update
+	}
+};
+
+const game = new Phaser.Game(config);
